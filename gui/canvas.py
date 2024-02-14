9f12230244110904229f55a2666227a0c3fe3b47
WinterYana <79871074+WinterYana@users.noreply.github.com>
2024-02-14 21:11:04 +0300

    Update canvas.py
    Обновлен on_mouse_press (для рисования line); Добавлены mouseMoveEvent, draw_line, setDrawingMode (для переключения между point и line), pan (для перемещения холста)

--- gui/canvas.py
+++ gui/canvas.py
@@ -13,6 +13,7 @@ class Canvas(FigureCanvas):
         super().__init__(fig)
         self.setParent(parent)
         self.data_manager = data_manager
+        self.drawing_mode = 'points'
 
         # Настройка осей и сетки для рисования (plt)
         self.ax.set_xlim([-10, 10])
@@ -25,6 +26,7 @@ class Canvas(FigureCanvas):
         # Подключение обработки событий нажатия мыши
         self.mpl_connect('button_press_event', self.on_mouse_press)
 
+
     def draw_axes_with_arrows(self):
         # Добавление стрелок на оси
         self.ax.add_patch(FancyArrowPatch((0, -10), (0, 10),
@@ -32,13 +34,71 @@ class Canvas(FigureCanvas):
         self.ax.add_patch(FancyArrowPatch((-10, 0), (10, 0),
                                           arrowstyle='->', mutation_scale=10))
 
-    def on_mouse_press(self, event):
-        # Обработчик события нажатия мыши, добавляет точку и рисует её
-        if event.inaxes:
-            self.data_manager.add_point(event.xdata, event.ydata) # Добавление точки в менеджер данных
-            self.draw_point(event.xdata, event.ydata) # Рисование точки
-
     def draw_point(self, x, y):
         # Метод для рисования точки на холсте
         self.ax.plot(x, y, 'ro') # Рисование красной точки
         self.draw() # Обновление холста для отображения нарисованной точки
+
+    def on_mouse_press(self, event):
+        # Обработчик события нажатия мыши, добавляет точку или линию в зависимости от режима
+        if event.inaxes:
+            if self.drawing_mode == 'points':
+                # Режим добавления точек
+                self.data_manager.add_point(event.xdata, event.ydata)
+                self.draw_point(event.xdata, event.ydata)
+            elif self.drawing_mode == 'lines':
+                if hasattr(self, 'temp_line_start'):
+                    # Фиксация линии
+                    self.data_manager.add_line(self.temp_line_start, (event.xdata, event.ydata))
+                    self.draw_line(self.temp_line_start, (event.xdata, event.ydata))
+                    # Сброс состояния для новой линии
+                    del self.temp_line_start
+                    if hasattr(self, 'temp_line'):
+                        self.temp_line.remove()
+                        del self.temp_line
+                    self.temp_line = None
+                else:
+                    # Если это первый клик, сохраняем начальную точку линии
+                    self.temp_line_start = (event.xdata, event.ydata)
+
+    def mouseMoveEvent(self, event):
+        if hasattr(self, 'temp_line_start'):
+            coords = self.ax.transData.inverted().transform((event.x(), event.y()))
+            # print(coords)
+            end_pos = (coords[0], self.ax.get_ylim()[1] - coords[1] + self.ax.get_ylim()[0])
+
+            if self.temp_line is None:
+                # Если temp_line ещё не создана, создаём её здесь
+                self.temp_line, = self.ax.plot([self.temp_line_start[0], end_pos[0]],
+                                               [self.temp_line_start[1], end_pos[1]], 'r--')
+            else:
+                # Если temp_line уже существует, обновляем её данные
+                self.temp_line.set_data([self.temp_line_start[0], end_pos[0]],
+                                        [self.temp_line_start[1], end_pos[1]])
+            self.figure.canvas.draw_idle()
+
+    def draw_line(self, start, end):
+        # Рисует линию между двумя точками
+        self.ax.plot([start[0], end[0]], [start[1], end[1]], 'r-')
+        self.draw_idle()
+
+    def pan(self, direction):
+        xlim = self.ax.get_xlim()
+        ylim = self.ax.get_ylim()
+        delta = 1  # Значение сдвига может быть адаптировано
+
+        if direction == 'left':
+            self.ax.set_xlim([xlim[0] - delta, xlim[1] - delta])
+        elif direction == 'right':
+            self.ax.set_xlim([xlim[0] + delta, xlim[1] + delta])
+        elif direction == 'up':
+            self.ax.set_ylim([ylim[0] + delta, ylim[1] + delta])
+        elif direction == 'down':
+            self.ax.set_ylim([ylim[0] - delta, ylim[1] - delta])
+
+        self.draw()  # Обновление холста
+
+    def setDrawingMode(self, mode):
+        self.drawing_mode = mode
+        if mode == 'lines':
+            self.temp_line = None  # Временная линия для предварительного просмотра
