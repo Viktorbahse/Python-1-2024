3fd16a5f8f6e6a11f5bb6701a96262621c5a8849
WinterYana <79871074+WinterYana@users.noreply.github.com>
2024-02-14 21:12:19 +0300

    Update data_manager.py
    Добавлены add_line и get_lines для работы с line

--- gui/data_manager.py
+++ gui/data_manager.py
@@ -2,6 +2,7 @@ class DataManager:
     # Конструктор класса DataManager, инициализирует хранилище для точек
     def __init__(self):
         self.points = [] # Список для хранения точек
+        self.lines = []  # Добавляем список для хранения линий
 
     # Метод для добавления новой точки в список
     def add_point(self, x, y):
@@ -10,3 +11,11 @@ class DataManager:
     # Метод для получения списка всех добавленных точек
     def get_points(self):
         return self.points
+
+    def add_line(self, start, end):
+        # Добавляет линию в список, где линия представлена как пара точек
+        self.lines.append((start, end))
+
+    def get_lines(self):
+        # Возвращает список всех добавленных линий
+        return self.lines
