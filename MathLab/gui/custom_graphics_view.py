572ede6ca402c38161582850f01f3709c2602027
Winter_Yana <winteryana@yandex.ru>
2024-03-17 21:55:22 +0300

    Добавлена возможность рисовать многоугольники. Добавлен предпросмотр для многоугольника. Добавлена принадлежность точек/линий к объектам (owner) (Beta, сделано только для многоугольников)

--- MathLab/gui/custom_graphics_view.py
+++ MathLab/gui/custom_graphics_view.py
@@ -26,6 +26,13 @@ class CustomGraphicsView(QGraphicsView):
             self.scene().shapes_manager.clear_temp_lines()  # Очистка предыдущих временных линий
             self.scene().shapes_manager.add_temp_line(
                 Line(Point(self.temp_point[0], self.temp_point[1]), Point(logical_pos[0], logical_pos[1]), (37, 109, 133, 255)))
+        elif self.current_tool == 'Polygon' and hasattr(self, 'polygon_points') and self.polygon_points:
+            last_point = self.polygon_points[-1]
+
+            # Очищаем предыдущие временные линии
+            self.scene().shapes_manager.clear_temp_lines()
+            # Добавляем временную линию для предпросмотра
+            self.scene().shapes_manager.add_temp_line(Line(last_point, Point(logical_pos[0], logical_pos[1]), (37, 109, 133, 255)))
 
         self.scene().update_scene()
 
@@ -54,6 +61,34 @@ class CustomGraphicsView(QGraphicsView):
                 self.scene().shapes_manager.add_shape(
                     Line(Point(self.temp_point[0], self.temp_point[1]), Point(final_point[0], final_point[1])))
                 del self.temp_point
+        elif self.current_tool == 'Polygon':
+            if not hasattr(self, 'polygon_points'):
+                self.current_polygon = Polygon([])
+                self.polygon_points = []
+
+            # Если ближайшая точка найдена и это начальная точка многоугольника, то завершаем рисование
+            if closest_point and self.polygon_points and closest_point == self.polygon_points[0]:
+                if len(self.polygon_points) > 1:  # Создаем линию от последней точки к первой
+                    last_point = self.polygon_points[-1]
+                    self.scene().shapes_manager.add_shape(Line(last_point, closest_point, owner=[self.current_polygon]))
+                self.scene().shapes_manager.clear_temp_lines()
+                self.scene().shapes_manager.add_shape(self.current_polygon)
+                delattr(self, 'polygon_points')
+            else:
+                if closest_point:
+                    new_point = closest_point
+                    new_point.add_to_owner(self.current_polygon)
+                else:
+                    new_point = Point(logical_pos[0], logical_pos[1], owner=[self.current_polygon])
+                    self.scene().shapes_manager.add_shape(new_point)
+                if not self.polygon_points or (self.polygon_points and new_point != self.polygon_points[-1]):
+                    if self.polygon_points:
+                        last_point = self.polygon_points[-1]
+                        self.scene().shapes_manager.add_shape(Line(last_point, new_point, owner=[self.current_polygon]))
+                    self.polygon_points.append(new_point)
+                    self.current_polygon.add_point(new_point)
+
+            self.scene().shapes_manager.clear_temp_lines()
         else:
             # Добавление и удаление точки
             if event.button() == Qt.LeftButton:
@@ -83,7 +118,12 @@ class CustomGraphicsView(QGraphicsView):
         elif event.key() == Qt.Key_D:
             self.scene().base_point[0] -= step
         elif event.key() == Qt.Key_Q:
-            self.current_tool = 'Line' if self.current_tool == 'Point' else 'Point'
+            if self.current_tool == 'Point':
+                self.current_tool = 'Line'
+            elif self.current_tool == 'Line':
+                self.current_tool = 'Polygon'
+            else:
+                self.current_tool = 'Point'
 
         self.scene().update_scene()
         super().keyPressEvent(event)
