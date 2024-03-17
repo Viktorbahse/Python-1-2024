55eb0bde0381c57a8098c3150614eae65fc44d37
Winter_Yana <winteryana@yandex.ru>
2024-03-17 04:25:33 +0300

    Обновлен color в __init__, теперь используется кортеж RGBA.

--- MathLab/core/geometric_objects/geom_obj.py
+++ MathLab/core/geometric_objects/geom_obj.py
@@ -2,12 +2,8 @@ import sympy as sp
 import math
 
 
-
-
-
-
 class Shape:
-    def __init__(self, color="black"):
+    def __init__(self, color=(6, 40, 61, 255)):  # rgb + прозрачность
         self.color = color
 
     def get_color(self):
@@ -16,17 +12,20 @@ class Shape:
     def set_color(self, color):
         self.color = color
 
+
 class Point(Shape):
-    def __init__(self, x, y, color="green"):
+    def __init__(self, x, y, color=(71, 181, 255, 255)):
         super().__init__(color)
         self.point = sp.Point(x, y)
         self.radius = 5
 
     def distance(self, other_point):
         return self.point.distance(other_point.point)
+
     def contains_point(self, x, y, check_radius=5):
         distance = math.sqrt((self.point.x - x) ** 2 + (self.point.y - y) ** 2)
         return distance <= check_radius
+
 
 # class Line(Shape):
 #     def __init__(self, x1, y1, x2, y2, color="black"):
@@ -35,14 +34,17 @@ class Point(Shape):
 #         self.y1 = y1
 #         self.x2 = x2
 #         self.y2 = y2
+
+
 class Line(Shape):
-    def __init__(self, a: Point,  b: Point, color="black"):
+    def __init__(self, a: Point,  b: Point, color=(6, 40, 61, 255), width=1.5):
         super().__init__(color)
         self.line = sp.Line(a.point, b.point)
         self.x1 = a.point.x
         self.y1 = a.point.y
         self.x2 = b.point.x
         self.y2 = b.point.y
+        self.width = width
 
 
 # class Line(Shape):
