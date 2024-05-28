e033edc309b1cf5eae2240a8119ea826f58a5732
Ekaterina Stakhova <starletkate@gmail.com>
2024-05-28 23:32:38 +0300

    final corrects after merge

--- MathLab/core/shapes_manager.py
+++ MathLab/core/shapes_manager.py
@@ -1,10 +1,9 @@
 from sympy import symbols, lambdify
-from PyQt5.QtCore import pyqtSignal, QObject
 from core.geometric_objects.figure import *
 from core.geometric_objects.geom_obj import *
+from tests.timing import *
 
 SEARCH_RADIUS = 5
-
 
 class Communicate(QObject):
     shapesChanged = pyqtSignal()
@@ -12,32 +11,29 @@ class Communicate(QObject):
 class ShapesManager:
     def __init__(self):
         self.comm = Communicate()
-        self.functions = []  # Массив для функций.
         # Словарик для хранения фигур
         self.shapes = {Point: [], Segment: [], Polygon: [], Line: [], Ray: [], Circle: [], Info: []}
         # Словарик для временных фигур
+        self.functions = []  # Массив для функций.
         self.temp_shapes = {Segment: [], Line: [], Ray: [], Circle: []}
-
         self.selected_points = []
 
     def add_shape(self, shape):
+        if type(shape) == Point:
+            shape.creating_name(self.shapes[Point])
         self.shapes[type(shape)].append(shape)
         self.comm.shapesChanged.emit()
 
     def remove_shape(self, shape):
-        changes = False
         if shape in self.shapes[type(shape)]:
-            if shape == Point:  # Метод удаления работает только для Point
-                self.shapes[type(shape)].remove(shape)
-                changes = True
+            self.shapes[type(shape)].remove(shape)
+            changes = True
+        if type(shape) != Point:
+            shape.__del__()
+            changes = True
         if changes:
             self.comm.shapesChanged.emit()
 
-    def find_shape(self, x, y, radius=SEARCH_RADIUS):
-        for shape in self.shapes[Point]:
-            if shape.contains_point(x, y, radius):
-                return shape
-        return
 
     def add_temp_shape(self, shape):
         shape_type = type(shape)
@@ -50,25 +46,36 @@ class ShapesManager:
             for shapes in self.temp_shapes.values():
                 shapes.clear()
 
-    def find_closest_point(self, x, y, radius=SEARCH_RADIUS):
-        # Поиск ближайшей точки в заданном радиусе
-        for shape in self.shapes[Point]:
-            if shape.contains_point(x, y, radius):
-                return shape
-        return None
-
-    @staticmethod
-    def distance(array_points: []):
-        a = array_points[0]
-        b = array_points[1]
-        dist = ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5
-        return dist
-
     def add_selected_point(self, point):
         self.selected_points.append(point)
 
     def clear_selected_points(self):
         self.selected_points = []
 
-# TODO: Перемести add_selected_point и clear_selected_points после clear_temp_shapes.
-# Хочу, чтобы у нас была некоторая логика в порядке функций (когда я меняю порядок, то функции записываются на меня)
+    def find_closest_shape(self, x, y, radius=SEARCH_RADIUS):
+        # Находит ближайший объект и расстояние до него
+        closest_shape = None
+        min_distance = float('inf')
+        all_shapes = []
+
+        for shape_list in self.shapes.values():
+            for shape in shape_list:
+                if isinstance(shape, Info):  # Пока костыль
+                    continue
+                if shape.invisible:  # Если объект невидимый, то пользователь с ним никак не взаимодействует
+                    continue
+                distance = shape.distance_to_shape(x, y)  # Этот метод должен вычислять расстояние от точки до объекта
+                if distance < radius:
+                    all_shapes.append(shape)
+                    if distance < min_distance and not isinstance(shape, Polygon):
+                        min_distance = distance
+                        closest_shape = shape
+
+        return closest_shape, min_distance, all_shapes
+
+    @staticmethod
+    def distance(array_points: []):
+        a = array_points[0]
+        b = array_points[1]
+        dist = ((a.entity.x - b.entity.x) ** 2 + (a.entity.y - b.entity.y) ** 2) ** 0.5
+        return dist
